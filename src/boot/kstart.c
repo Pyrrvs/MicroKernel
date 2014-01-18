a2ef07cf6524f6e0ffe1e241989c66eb3ba01b8f
José Rodrigues <contact.jrodrigues@gmail.com>
2014-01-18 03:22:09 +0100

    var args finished

--- src/boot/kstart.c
+++ src/boot/kstart.c
@@ -16,11 +16,15 @@ void gp_handler(registers_t *regs)
 
 int kstart(int code, multiboot_info_t * mBootInfo)
 {
+  printk("\n\n [ %s ]  = %d \n\n", "La vie L'univers et tout le reste", 42);
+
   if (code != MULTIBOOT_BOOTLOADER_MAGIC)
-    {
-      PANIC("Kernel need to be loaded by a multiboot"
-	    " compliant bootloader\n");
-    }
+  {
+    PANIC("Kernel need to be loaded by a multiboot"
+	" compliant bootloader\n");
+  }
+
+
   printk("Kernel up and running\n");
   isr_register(0xD, gp_handler);
   printk(WARN_COLOR "Testing interrupts\n" DEF_COLOR);
@@ -30,9 +34,11 @@ int kstart(int code, multiboot_info_t * mBootInfo)
   isr_unregister(0x0);
 
   printk(WARN_COLOR
-	 "Testing page fault - (Writing on RO Page)\n"
-	 DEF_COLOR);
+      "Testing page fault - (Writing on RO Page)\n"
+      DEF_COLOR);
   char *str = (char*)0xC03FF000;
   str[0] = 1;
+
+
   return 0xBABA;
 }
--- src/common/printk.c
+++ src/common/printk.c
@@ -5,36 +5,34 @@
 
 void printk(char const *s, ...)
 {
-  va_list args;
-  unsigned int i, len;
-  int val;
-  char *str;
+	va_list args;
+	unsigned int i, len;
+	int val;
+	char *str;
 
-  len = strlen(s);
-  va_start(args, s);
-  for (i = 0; i < len; ++i)
-    {
-      if (s[i] == '%')
-	     {
-	       i = i + 1;
-	       switch (s[i])
-          {
-      case 'd':
-	      val = va_arg(args, int);
-	      putnbr(val);
-	      break;
-	    case 's':
-	      
-	      str = va_arg(args, char*);
-	      putnbr((int)str);
-	      puts(str);
-	      break;
-	    default:
-	      putc(s[i]);
-	      break;
-	    }
+	len = strlen(s);
+	va_start(args, s);
+	for (i = 0; i < len; ++i)
+	{
+		if (s[i] == '%')
+		{
+			i = i + 1;
+			switch (s[i])
+			{
+				case 'd':
+					val = va_arg(args, int);
+					putnbr(val);
+					break;
+				case 's':
+					str = va_arg(args, char*);
+					puts(str);
+					break;
+				default:
+					putc(s[i]);
+					break;
+			}
+		}
+		else
+			putc(s[i]);
 	}
-      else
-		putc(s[i]);
-    }
 }
