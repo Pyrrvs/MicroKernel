54109a9d81cf0ed58a80c9f04f6052351fb8f632
Gauthier Lamaison <gauthier.lamaison@gmail.com>
2014-01-30 18:06:07 +0100

    working on multiboot structure

---
+++ src/mm/mm.c
@@ -0,0 +1,67 @@
+#include "3rdparty/multiboot.h"
+#include "common/stdio.h"
+#include "mm/mm.h"
+#include "common/kernel.h"
+
+mem_info_t g_mem_info;
+
+int mm_init(multiboot_info_t * mboot_info)
+{
+  if (mboot_info->flags & MULTIBOOT_INFO_MEMORY == 0)
+    {
+      PANIC("No memory information available in the multiboot structure\n");
+    }
+
+  
+  if (mboot_info->flags & MULTIBOOT_INFO_CMDLINE)
+    {
+      printk(INFO_COLOR "Command line: " DEF_COLOR
+	     "[%s]\n", mboot_info->cmdline + KERNEL_VIRTUAL_BASE);
+    }
+  
+
+  if (mboot_info->flags & MULTIBOOT_INFO_MODS)
+    {
+      printk(INFO_COLOR "Modules info: " DEF_COLOR
+	     "count [%u]\n", mboot_info->mods_count);
+
+      MODULES_FOREACH(mod, (mboot_info->mods_addr + KERNEL_VIRTUAL_BASE), mboot_info->mods_count)
+	{
+	  printk("\tModule start at [%x] and ends at [%x] ; cmdline [%s]\n", \
+		 mod->mod_start, mod->mod_end, mod->cmdline + KERNEL_VIRTUAL_BASE);
+	}
+    }
+  
+
+  if (mboot_info->flags & MULTIBOOT_INFO_MEM_MAP)
+    {
+      printk(INFO_COLOR "Memory map: " DEF_COLOR
+	     "length [%u]\n", mboot_info->mmap_length);
+
+      MMAP_FOREACH(ent, mboot_info->mmap_addr, mboot_info->mmap_length)
+	{
+      	  printk("\tEntry: size [%u] ;  addr [%x] ; len [%u] ; type [%x]",
+      		 ent->size, ent->addr, ent->len, ent->type);
+      	  if (ent->type & MULTIBOOT_MEMORY_AVAILABLE)
+      	    printk(" ; available");
+      	  if (ent->type & MULTIBOOT_MEMORY_RESERVED)
+      	    printk(" ; reserved");
+      	  printk("\n");
+	}
+    }
+
+  g_mem_info.phys.total_size = mboot_info->mem_upper * 1024 + 1;
+  g_mem_info.phys.higher_addr = mboot_info->mem_upper * 1024;
+  printk(INFO_COLOR "Multiboot memory info:\n" DEF_COLOR);
+  printk("\tmapping: lower [%x - %x] ; upper [%x - %x]\n",
+         0,
+         mboot_info->mem_lower * 1024 - 1,
+         mboot_info->mem_lower * 1024,
+         g_mem_info.phys.higher_addr - 1);
+  printk("\tavailable size: lower %u KB ; upper %u MB ; total size %u MB\n",
+         mboot_info->mem_lower,
+         (mboot_info->mem_upper - mboot_info->mem_lower) / 1024,
+         g_mem_info.phys.total_size / 1024 / 1024);
+  return 0;
+}
+
---
+++ src/mm/mm.h
@@ -0,0 +1,18 @@
+#ifndef		MM_H_
+# define	MM_H_
+
+# include "common/types.h"
+# include "3rdparty/multiboot.h"
+
+typedef struct mem_info
+{
+  struct {
+    uint32_t total_size;
+    uint32_t higher_addr;
+  } phys;
+} mem_info_t;
+
+extern mem_info_t g_mem_info;
+int mm_init(multiboot_info_t * mboot_info);
+
+#endif
